1844ebe2717be282ada913260e6d6cf8ac28b186
rajeevdhar <rajeev.dhar@gmail.com>
2014-12-15 14:53:23 -0800

    update app controller

--- micopenly/app/controllers/articles_controller.rb
+++ micopenly/app/controllers/articles_controller.rb
@@ -4,7 +4,10 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(params[:article])
+
+    @article.save
+    redirect_to @article
   end
 
 end
